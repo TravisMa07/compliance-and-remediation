cef4a4413568cc1f12378844ac68e83c0350b627
TravisMa07 <travis07ma@gmail.com>
2026-01-20 12:14:07 -0500

    Create DISA-STIG-Template.ps1

---
+++ Compliance Audits/DISA-STIG/DISA-STIG-Template.ps1
@@ -0,0 +1,34 @@
+<#
+.SYNOPSIS
+    This PowerShell script ensures that the associated STIG-ID vulnerability is remediated.
+
+.NOTES
+    Author          : Travis Ma
+    LinkedIn        : https://www.linkedin.com/in/travis-maa/
+    GitHub          : https://github.com/travisma07
+    Date Created    : ../../....
+    Last Modified   : ../../....
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : 
+
+.DESCRIPTION
+    This script sets the following registry value:
+
+
+    This enforces:
+
+
+.TESTED ON
+    Date(s) Tested  : ../../....
+    Tested By       : Travis Ma
+    Systems Tested  : 
+    PowerShell Ver. : 5.1
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\stig_remediation_template(STIG-ID-).ps1
+#>
+
+# YOUR CODE GOES HERE
